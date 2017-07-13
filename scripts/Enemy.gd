600e7816621047349419d8b90389f347bbca3c82
Bastien Duplessier <bastien.duplessier@gmail.com>
2017-07-13 18:48:07 +0200

    Go to gameover when meteor touches ship

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+signal game_over
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -14,6 +15,8 @@ func _ready():
 func _on_Enemy_body_enter( body ):
 	if body.get_name() == "Limits":
 		self.queue_free()
+	if body.get_name() == "Player":
+		emit_signal("game_over")
 	if body.get_name().begins_with("Shoot"):
 		body.queue_free()
 		self.queue_free()
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -11,9 +11,12 @@ func _ready():
 	# Initialization here
 	pass
 
-
 func _on_Timer_timeout():
 	var meteor = preload("res://Enemy.tscn").instance()
 	meteorCount = meteorCount + 1
 	meteor.set_name("Meteor" + str(meteorCount))
+	meteor.connect("game_over", self, "run_game_over")
 	add_child(meteor)
+
+func run_game_over():
+	get_tree().change_scene_to(preload("res://GameOver.tscn"))
